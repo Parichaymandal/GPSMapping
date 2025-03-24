235a948c2fa722053672bc197dbb1601d8d36bc8
Parichaymandal <parichay120201@gmail.com>
2025-03-24 10:54:27 +0100

    Environment file added and configuring database.

--- database.py
+++ database.py
@@ -1,10 +1,19 @@
+import os
 import sqlalchemy as alchemy
 import sqlalchemy.ext.declarative as declarative
 import sqlalchemy.orm as orm
+from dotenv import load_dotenv
 
-DB_URL = 'postgresql://user:password@localhost:5432/roadnet_db'
-engine = alchemy.create_engine(DB_URL, connect_args={"check_same_thread":False})
+load_dotenv()
 
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432") 
+
+DB_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+engine = alchemy.create_engine(DB_URL)
 session = orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative.declarative_base()
+
