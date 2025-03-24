59d33ef92841faf46310b3c46078d5ec2673d03f
Parichaymandal <parichay120201@gmail.com>
2025-03-24 10:55:48 +0100

    Defining data models

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 import sqlalchemy as alchemy
-from sqlalchemy import Column, BigInteger, Sequence, ForeignKey, ARRAY
+from sqlalchemy import Column, BigInteger, Sequence, ForeignKey, ARRAY, PrimaryKeyConstraint, TIMESTAMP
 import sqlalchemy.ext.declarative as declarative
 import sqlalchemy.orm as orm
+from sqlalchemy.orm import relationship
 from geoalchemy2 import Geometry
 
 
@@ -11,27 +12,28 @@ import database as db
 
 class Recording(db.Base):
     __tablename__ = "recordings"
-    #id=
-    recording_id = Column(BigInteger, Sequence('recording_id_seq'), primary_key=True)
+    id = Column(BigInteger, Sequence('recording_id'), primary_key=True)
+    timestamp = Column(TIMESTAMP, nullable=False)
+    gps_tracks = relationship("GPSTrack", back_populates="recording")
+
+class GPSTrack(db.Base):
+    __tablename__ = "gps_tracks" 
+    id = Column(BigInteger,  Sequence('track_id'), primary_key=True)
+    recording_id = Column(BigInteger, ForeignKey("recordings.id"), nullable=False)
     gps_index = Column(BigInteger, nullable=False)
-    geometry = Column(Geometry('POINT', srid=4326, spatial_index=True))
+    geometry = Column(Geometry("POINT", srid=4326), nullable=False)
+    recording = relationship("Recording", back_populates="gps_tracks")
+
 
 class RoadEdge(db.Base):
-    __tablename__ = 'road_edges'
-    
-    edge_id = Column(BigInteger, Sequence('edge_id_seq'), primary_key=True)
-    start_node = Column(BigInteger, nullable=False)
-    end_node = Column(BigInteger, nullable=False)
-    start_geometry = Column(Geometry('POINT', srid=4326))
-    end_geometry = Column(Geometry('POINT', srid=4326))
+    __tablename__ = 'road_edges' 
+    id = Column(BigInteger, Sequence('edge_id'), primary_key=True)
     geometry = Column(Geometry('LINESTRING', srid=4326))
 
 class GPSEdgeMapping(db.Base):
     __tablename__ = 'gps_edge_mapping'
-    
-    edge_id = Column(BigInteger, nullable=False)
-    recording_id = Column(BigInteger, nullable=False)
-    gps_index_array = Column(BigInteger, nullable=False)
-    
+    edge_id = Column(BigInteger, ForeignKey('road_edges.id'), nullable=False)
+    recording_id = Column(BigInteger, ForeignKey('recordings.id'), nullable=False)
+    gps_index_array = Column(ARRAY(BigInteger), nullable=False)
     __table_args__ = (PrimaryKeyConstraint('edge_id', 'recording_id'),)
 
